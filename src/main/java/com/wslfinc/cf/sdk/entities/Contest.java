a156ac4ddc25345b15ab5afe26c0d926a27f0396
WslF <WasylFranchuk@gmail.com>
2017-02-01 17:10:18 +0200

    make comparable

--- src/main/java/com/wslfinc/cf/sdk/entities/Contest.java
+++ src/main/java/com/wslfinc/cf/sdk/entities/Contest.java
@@ -7,7 +7,7 @@ import org.json.JSONObject;
  *
  * @author Wsl_F
  */
-public class Contest {
+public class Contest implements Comparable<Contest> {
 
     /**
      * Integer.
@@ -101,7 +101,7 @@ public class Contest {
      * String. Can be absent.
      */
     String season;
-    
+
     public Contest(int id, String name, ContestType type, ContestPhase phase, boolean frozen, int durationSeconds, int startTimeSeconds, int relativeTimeSeconds, String preparedBy, String websiteUrl, String description, int difficulty, String kind, String icpcRegion, String country, String city, String season) {
         this.id = id;
         this.name = name;
@@ -121,11 +121,11 @@ public class Contest {
         this.city = city;
         this.season = season;
     }
-    
+
     public Contest(JSONObject contest) {
         setAll(contest);
     }
-    
+
     public final void setAll(JSONObject contest) {
         this.id = JsonExtractor.getInt(contest, "id");
         this.name = JsonExtractor.getString(contest, "name");
@@ -145,141 +145,154 @@ public class Contest {
         this.city = JsonExtractor.getString(contest, "city");
         this.season = JsonExtractor.getString(contest, "season");
     }
-    
+
     public int getId() {
         return id;
     }
-    
+
     public void setId(int id) {
         this.id = id;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
-    
+
     public ContestType getType() {
         return type;
     }
-    
+
     public void setType(ContestType type) {
         this.type = type;
     }
-    
+
     public ContestPhase getPhase() {
         return phase;
     }
-    
+
     public void setPhase(ContestPhase phase) {
         this.phase = phase;
     }
-    
+
     public boolean isFrozen() {
         return frozen;
     }
-    
+
     public void setFrozen(boolean frozen) {
         this.frozen = frozen;
     }
-    
+
     public int getDurationSeconds() {
         return durationSeconds;
     }
-    
+
     public void setDurationSeconds(int durationSeconds) {
         this.durationSeconds = durationSeconds;
     }
-    
+
     public long getStartTimeSeconds() {
         return startTimeSeconds;
     }
-    
+
     public void setStartTimeSeconds(long startTimeSeconds) {
         this.startTimeSeconds = startTimeSeconds;
     }
-    
+
     public int getRelativeTimeSeconds() {
         return relativeTimeSeconds;
     }
-    
+
     public void setRelativeTimeSeconds(int relativeTimeSeconds) {
         this.relativeTimeSeconds = relativeTimeSeconds;
     }
-    
+
     public String getPreparedBy() {
         return preparedBy;
     }
-    
+
     public void setPreparedBy(String preparedBy) {
         this.preparedBy = preparedBy;
     }
-    
+
     public String getWebsiteUrl() {
         return websiteUrl;
     }
-    
+
     public void setWebsiteUrl(String websiteUrl) {
         this.websiteUrl = websiteUrl;
     }
-    
+
     public String getDescription() {
         return description;
     }
-    
+
     public void setDescription(String description) {
         this.description = description;
     }
-    
+
     public int getDifficulty() {
         return difficulty;
     }
-    
+
     public void setDifficulty(int difficulty) {
         this.difficulty = difficulty;
     }
-    
+
     public String getKind() {
         return kind;
     }
-    
+
     public void setKind(String kind) {
         this.kind = kind;
     }
-    
+
     public String getIcpcRegion() {
         return icpcRegion;
     }
-    
+
     public void setIcpcRegion(String icpcRegion) {
         this.icpcRegion = icpcRegion;
     }
-    
+
     public String getCountry() {
         return country;
     }
-    
+
     public void setCountry(String country) {
         this.country = country;
     }
-    
+
     public String getCity() {
         return city;
     }
-    
+
     public void setCity(String city) {
         this.city = city;
     }
-    
+
     public String getSeason() {
         return season;
     }
-    
+
     public void setSeason(String season) {
         this.season = season;
     }
-    
+
+    @Override
+    public int compareTo(Contest contest) {
+        if (this.id == contest.id) {
+            return 0;
+        }
+
+        if (this.startTimeSeconds == contest.startTimeSeconds) {
+            return this.id < contest.id ? -1 : 1;
+        }
+
+        return this.startTimeSeconds < contest.startTimeSeconds ? -1 : 1;
+    }
+
 }
