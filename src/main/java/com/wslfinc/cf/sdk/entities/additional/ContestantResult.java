7db7fc85fdaaa468e5f01a9dd6ec3709cb2a0ae0
WslF <WasylFranchuk@gmail.com>
2017-02-06 00:45:22 +0200

    add conversion to JSON

--- src/main/java/com/wslfinc/cf/sdk/entities/additional/ContestantResult.java
+++ src/main/java/com/wslfinc/cf/sdk/entities/additional/ContestantResult.java
@@ -1,5 +1,11 @@
 package com.wslfinc.cf.sdk.entities.additional;
 
+import static com.wslfinc.cf.sdk.Constants.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  *
  * @author Wsl_F
@@ -34,5 +40,37 @@ public class ContestantResult {
 
     public int getPrevRating() {
         return contestant.prevRating;
+    }
+
+    public JSONObject toJSON() {
+        JSONObject json = new JSONObject();
+        json.put("handle", contestant.handle);
+        json.put("rank", contestant.rank);
+        json.put("oldRating", contestant.prevRating);
+        json.put("newRating", nextRating);
+        json.put("seed", seed);
+        return json;
+    }
+
+    public JSONObject deltasToJSON() {
+        JSONObject json = new JSONObject();
+        json.put("handle", contestant.handle);
+        int delta = nextRating - contestant.prevRating;
+        json.put("delta", delta);
+        return json;
+    }
+
+    public static JSONObject toJSON(List<ContestantResult> nextRating) {
+        List<JSONObject> list = new ArrayList<>(nextRating.size());
+        for (ContestantResult result : nextRating) {
+            list.add(result.toJSON());
+        }
+
+        JSONArray array = new JSONArray(list);
+        JSONObject json = new JSONObject();
+        json.put("status", SUCCESSFUL_STATUS);
+        json.put(JSON_RESULTS, array);
+
+        return json;
     }
 }
