51e3b6552cb54fb794ccb7068affd929868396ff
WslF <WasylFranchuk@gmail.com>
2017-02-07 02:52:51 +0200

    get response directly from servlet

--- src/main/java/com/wslfinc/servlets/GetNextRatingServlet.java
+++ src/main/java/com/wslfinc/servlets/GetNextRatingServlet.java
@@ -2,12 +2,11 @@ package com.wslfinc.servlets;
 
 import com.wslfinc.cf.sdk.CodeForcesSDK;
 import com.wslfinc.cf.sdk.entities.additional.ContestantResult;
-import java.util.ArrayList;
+import java.io.PrintWriter;
 import java.util.List;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -18,7 +17,8 @@ public class GetNextRatingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-
+        System.out.println("Request received!");
+        System.out.println("Parameter: " + request.getParameter("contestId"));
         int contestId = Integer.valueOf(request.getParameter("contestId"));
 
         List<ContestantResult> nextRating = CodeForcesSDK.getNewRatings(contestId);
@@ -26,7 +26,10 @@ public class GetNextRatingServlet extends HttpServlet {
         request.setAttribute("nextRating", json);
 
         try {
-            request.getRequestDispatcher("NextRatingJSON.jsp").forward(request, response);
+            PrintWriter out = response.getWriter();
+            out.write(json.toString());
+            out.flush();
+            response.setStatus(200);
         } catch (Exception ex) {
             System.err.println(ex.getMessage());
         }
