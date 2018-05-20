31094272626bae1a2897fd7f4d40d91f5ef34219
Vasyl Franchuk <WasylF@gmail.com>
2018-05-20 16:21:11 +0300

    update extension: colored icon + question mark for unknown deltas

--- CF-Predictor-Extension/showDeltas.js
+++ CF-Predictor-Extension/showDeltas.js
@@ -1,29 +1,33 @@
 var partyNum = 0;
-var deltas = [];
+var results = [];
 
 function modifyPartyHtml(index, elem) {
-	var delta = 0;
+	var delta = '?';
+	var rank = ' ';
+	var seed = ' ';
 
 	if (partyNum > 0) {
-		var handle = $(elem).find("td:eq(1)").find("a").first().html();
+		var handle = $(elem).find("td:eq(1)").find("a").last().html();
 		if (handle) {
 			//next 2 lines - fix for legendary grandmaster
 			handle = handle.replace('<span class="legendary-user-first-letter">','');
 			handle = handle.replace('</span>','');
-			if (handle in deltas) {
-				delta = Math.round(deltas[handle]);
+			if (handle in results) {
+				delta = results[handle].delta;
+				rank = results[handle].rank;
+				seed = results[handle].seed;
 			}
 		}
 	}
 
+	var darkClass = "";
+	if (partyNum % 2 == 1) {
+		darkClass = "dark ";
+	}
 	var text;
 	if (partyNum == 0) {
 		text = "<th class='top right' style='width: 4em;'><span title='Rating change''>&Delta;</span></th>";
 	} else {
-		var darkClass = "";
-		if (partyNum % 2 == 1) {
-			darkClass = "dark ";
-		}
 		if (delta > 0) {
 			text = "<td class='" + darkClass + "right'><span style='color:green;font-weight:bold;'>+" + delta + "</span></td>";
 		} else {
@@ -33,6 +37,12 @@ function modifyPartyHtml(index, elem) {
 
 	partyNum++;
 	$(elem).append(text);
+	/*
+	text = "<td class='" + darkClass + "right'><span style='color:green;'>" + rank + "</span></td>";
+	$(elem).append(text);
+	text = "<td class='" + darkClass + "right'><span style='color:green;'>" + seed + "</span></td>";
+	$(elem).append(text);
+	*/
 }
 
 function showDeltas() {
@@ -44,45 +54,36 @@ function showDeltas() {
 			$(".standings").find("tr").find("td").removeClass("right");
 			$(".standings").find("tr").each(modifyPartyHtml);
 			if (count % 2 == 0) {
-				$(".standings").find("tr").last().find("td").last().replaceWith("<td class='smaller bottom dark right'> </td>");
+				$(".standings").find("tr").last().find("td").last().replaceWith("<td class='smaller bottom right dark'>&Delta;</td>");
 			} else {
-				$(".standings").find("tr").last().find("td").last().replaceWith("<td class='smaller bottom right'> </td>");
+				$(".standings").find("tr").last().find("td").last().replaceWith("<td class='smaller bottom right'>&Delta;</td>");
 			}
 		});
 	}
 }
 
-function urlExists(url, callback){
-	$.ajax({
-		type: 'HEAD',
-		url: url,
-		success: function(){
-			callback(true);
-		},
-		error: function() {
-			callback(false);
-		}
-	});
-}
-
-
 function getDeltas(contestId, callback) {
 	//var localServer = "http://localhost:8084/CF-PredictorFrontEnd/"
-	var amazonServer = "http://cf-predictor.us-west-2.elasticbeanstalk.com/";
 	var herokuServer = "https://cf-predictor-frontend.herokuapp.com/";
 	var page = 	"GetNextRatingServlet?contestId=" + contestId;
 
-	urlExists(amazonServer, function(exists) {
-		var server = (exists ? amazonServer : herokuServer) + page;
+		var server = herokuServer + page;
 		$.getJSON(server, function(data) {
 			for (var i = 0; i < data.result.length; i++) {
 				var handle = data.result[i].handle;
 				var delta = data.result[i].newRating - data.result[i].oldRating;
-				deltas[handle] = delta;
+				var rank = data.result[i].rank;
+				var seed = data.result[i].seed;
+
+				var res = {
+					delta : parseInt(delta),
+					seed : parseInt(seed),
+					rank : parseInt(rank)
+				};
+				results[handle] = res;
 			}
 			callback();
 		});
-	});
 }
 
 
